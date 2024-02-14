0372e076158a96e7d7e1f44b9ee6ced2931fe1ec
beso <157532171+591006459@users.noreply.github.com>
2024-02-14 18:18:58 +0400

    first commit

--- day1/lesson.1.py
+++ day1/lesson.1.py
@@ -1,92 +1,16 @@
-from turtle import* 
+name = "beso"
 
-#we want to paint a hause
+surname = "apkhadze"
 
-#step 1: draw a square
-speed(30)
-width(7)
-color("red")
-forward(200)
-left(90)
 
-forward(200)
-left(90)
+age = 19
+height = 178.6
 
-forward(200)
-left(90)
-
-forward(200)
-left(90)
+know_programing = True
+is_ugly = False
 
 
 
 
-
-
-
-
-#end of square
-
-#drawing a door
-
-forward(70)
-color("black")
-begin_fill()
-left(90)
-forward(120)     #height of the door
-right(90)
-forward(60)
-right(90)
-forward(120)
-end_fill()
-
-penup()
-goto(200, 200)
-pendown()
-
-color("grey")
-begin_fill()
-right(150)
-forward(200)
-left(120)
-forward(200)
-end_fill()
-#end of door
-
-#drawing a window
-
-penup()
-goto(0, 130)
-pendown()
-color("brown")
-begin_fill()
-left(120)
-forward(60)
-left(90)
-forward(60)
-left(90)
-forward(60)
-left(90)
-forward(60)
-end_fill()
-
-penup()
-goto(180, 130)
-pendown()
-begin_fill()
-left(90)
-forward(20)
-left(90)
-forward(60)
-left(90)
-forward(60)
-left(90)
-forward(60)
-left(90)
-forward(40)
-end_fill()
-
-
-
-exitonclick()
-
+print(name +" " + surname  +" " +str(age)  )
+ 
